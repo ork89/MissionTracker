a29fd3f296584c411ea1c356bfd939cc41ac3bc3
ork89 <kosman.ori@gmail.com>
2021-01-25 19:03:49 +0200

    Added Delete functionality

--- src/containers/TaskItemList/TaskItemList.js
+++ src/containers/TaskItemList/TaskItemList.js
@@ -6,17 +6,19 @@ import classes from './TaskItemList.module.css';
 const TaskItemList = props => {
 	const [tasksList, setTasksList] = useState([
 		{
+			id: 'aabbcc',
 			taskDescription: 'This is the first task of the project',
-			project: 'MisTracker',
+			project: 'MissionTracker',
 			startTime: '08:05:10',
 			endTime: '09:34:44',
-			totalTime: '01:29:44',
+			totalTime: '01:29:34',
 			date: new Date('2021-01-11T00:00:00'),
 			priority: 'low',
 		},
 		{
+			id: 'ddeeff',
 			taskDescription: 'Second Task',
-			project: 'MisTracker',
+			project: 'MissionTracker',
 			startTime: '09:34:44',
 			endTime: '10:02:39',
 			totalTime: '00:27:55',
@@ -24,8 +26,9 @@ const TaskItemList = props => {
 			priority: 'low',
 		},
 		{
+			id: 'gghhii',
 			taskDescription: 'Third Task',
-			project: 'MisTracker',
+			project: 'MissionTracker',
 			startTime: '10:02:39',
 			endTime: '13:04:11',
 			totalTime: '03:01:16',
@@ -34,13 +37,18 @@ const TaskItemList = props => {
 		},
 	]);
 
-	const inputChangedHandler = (event, inputId) => {};
+	// const inputChangedHandler = (event, inputId) => {};
+
+	const handleDelete = id => {
+		const newTasksList = tasksList.filter(item => item.id != id);
+		setTasksList(newTasksList);
+	};
 
 	return (
 		<div className={classes.TaskItemList}>
-			{tasksList.map((task, index) => (
+			{tasksList.map(task => (
 				<TaskItem
-					key={index}
+					key={task.id}
 					description={task.taskDescription}
 					project={task.project}
 					startTime={task.startTime}
@@ -48,6 +56,8 @@ const TaskItemList = props => {
 					totalTime={task.totalTime}
 					date={task.date}
 					priority={task.priority}
+					deleteItem={() => handleDelete(task.id)}
+					// onChange={inputChangedHandler}
 				/>
 			))}
 		</div>
