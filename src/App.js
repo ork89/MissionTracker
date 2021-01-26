f779d7d24cc3422176d2811ede5813bcb4d79aa0
ork89 <kosman.ori@gmail.com>
2021-01-26 17:23:28 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, { useEffect, Suspense } from 'react';
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
+import TaskItemList from './containers/TaskItemList/TaskItemList';
+import Projects from './containers/Projects/Projects';
 
 import './App.css';
 
+const projects = React.lazy(() => {
+	return import('./containers/Projects/Projects');
+});
+
 const App = props => {
+	let routes = (
+		<Switch>
+			<Route path='/' exact component={TaskItemList} />
+			<Route path='/Projects' component={Projects} />
+			<Redirect to='/' />
+		</Switch>
+	);
+
 	return (
 		<div className='App'>
-			<Layout />
-			{/* <Task />
-			<TaskItemList /> */}
+			<Layout>
+				<Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+			</Layout>
 		</div>
 	);
 };
