daf8bd9145c981e398e5be3bef6812dc54a7a3f3
ork89 <kosman.ori@gmail.com>
2021-01-26 17:26:42 +0200

    Added responsiveness for mobile devices

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-// import MissionTrackerLogo from '../../assets/hourglass_logo.png';
 import MissionTrackerLogo from '../../assets/MissionTrackerLogo.png';
+import ToggleLogo from '../../assets/ToggleLogo.png';
 import classes from './Logo.module.css';
 
 const Logo = () => (
-	<div className={classes.Logo}>
-		<img src={MissionTrackerLogo} alt='MissionTrackerLogo' />
-	</div>
+	<React.Fragment>
+		<div className={classes.Logo}>
+			<img src={MissionTrackerLogo} alt='MissionTrackerLogo' />
+		</div>
+		<div className={classes.ToggleLogo}>
+			<img src={ToggleLogo} alt='MissionTrackerLogo' />
+		</div>
+	</React.Fragment>
 );
 
 export default Logo;
--- src/components/Navigation/Menu/Menu.js
+++ src/components/Navigation/Menu/Menu.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Menu.module.css';
 
 const Menu = props => {
 	return (
 		<div className={classes.Menu}>
+			<DrawerToggle clicked={props.drawerToggleClicked} />
 			<div className={classes.logo}>
-				{/* <p>MissTracker</p> */}
 				<Logo />
 			</div>
-			<nav>
+			<nav className={classes.Desktop}>
 				<NavigationItems />
 			</nav>
 		</div>
