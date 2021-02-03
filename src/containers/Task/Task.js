11912011c60d5d31ea8a0dccac0380a7af11aeb1
ork89 <kosman.ori@gmail.com>
2021-02-03 19:13:46 +0200

    Added Axios and connected project to Firebase. Tasks are now fetched from DB and saved there.

--- src/containers/Task/Task.js
+++ src/containers/Task/Task.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from '../../axios';
 
 import Timer from '../../components/Timer/Timer';
 import TimerControls from '../../components/Timer/TimerControls/TimerControls';
 import TaskDescription from '../../components/Timer/TaskDescription/TaskDescription';
 import SelectInput from '../../components/UI/Input/SelectInput';
 import ProjectDialog from '../../components/UI/Dialog/ProjectDialog';
+import { startTimer, stopTimer, pauseTimer } from '../../store/actions/trackerActions';
 import classes from './Task.module.css';
 
 let incrementor;
@@ -12,7 +15,13 @@ let incrementor;
 const Task = () => {
 	const [secondsElapsed, setSecondsElapsed] = useState(0);
 	const [isStarted, setIsStarted] = useState(false);
+	const [startTime, setStartTime] = useState('');
+	const [endTime, setEndTime] = useState('');
 	const [inputSelectedValue, setInputSelectedValue] = useState();
+	const [description, setDescription] = useState('');
+	const [selectedProjectName, setSelectedProjectName] = useState('');
+	const dispatch = useDispatch();
+
 	const priorities = ['Non Issue', 'Low', 'Medium', 'High'];
 
 	const getSeconds = () => {
@@ -27,8 +36,34 @@ const Task = () => {
 		return `0${Math.floor(secondsElapsed / 3600)}`.slice(-2);
 	};
 
+	const createNewTaskInDB = () => {
+		const currentTime = `${getHours()}:${getMinutes()}:${getSeconds()}`;
+
+		const newTask = {
+			desc: description,
+			startTime,
+			endTime,
+			totalTime: currentTime,
+			date: new Date(),
+			project: selectedProjectName,
+			priority: inputSelectedValue,
+			user: {
+				name: 'Tester',
+				email: 'test@test.com',
+			},
+		};
+		axios
+			.post('/tasks.json', newTask)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
+	};
+
 	const timerStartedHandler = () => {
 		setIsStarted(true);
+		const dateTime = new Date();
+		const localStartTime = dateTime.toLocaleTimeString();
+		setStartTime(localStartTime);
+		dispatch(startTimer());
 
 		incrementor = setInterval(() => {
 			setSecondsElapsed(seconds => seconds + 1);
@@ -37,15 +72,31 @@ const Task = () => {
 
 	const timerPausedHandler = () => {
 		clearInterval(incrementor);
+		dispatch(pauseTimer());
 	};
+
 	const timerStoppedHandler = () => {
 		clearInterval(incrementor);
 		setSecondsElapsed(0);
 		setIsStarted(false);
+		dispatch(stopTimer());
+
+		const dateTime = new Date();
+		const localEndTime = dateTime.toLocaleTimeString();
+		setEndTime(localEndTime);
+		createNewTaskInDB();
 	};
 
 	const handleSelectedValue = value => {
 		setInputSelectedValue(value);
+	};
+
+	const handleDescriptionChanged = input => {
+		setDescription(input);
+	};
+
+	const handleProjectSelected = projectName => {
+		setSelectedProjectName(projectName);
 	};
 
 	return (
@@ -57,14 +108,14 @@ const Task = () => {
 				timerStopped={timerStoppedHandler}
 				disabled={!isStarted}
 			/>
-			<TaskDescription />
+			<TaskDescription input={handleDescriptionChanged} />
 			<div className={classes.projectAndPriority}>
 				<SelectInput
 					defaultValue='Low'
 					inputOptions={priorities}
 					selectedValue={handleSelectedValue}
 				/>
-				<ProjectDialog />
+				<ProjectDialog selectedProject={handleProjectSelected} />
 			</div>
 		</div>
 	);
--- src/containers/Tracker/Tracker.js
+++ src/containers/Tracker/Tracker.js
@@ -1,71 +1,96 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 
 import TaskItem from '../../components/TaskItem/TaskItem';
 import Task from '../Task/Task';
+import axios from '../../axios';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Tracker.module.css';
 
 const Tracker = () => {
 	// TODO: Replace hardcoded tasks with tasks form the DB
 	const [tasksList, setTasksList] = useState([
-		{
-			id: 'aabbcc',
-			taskDescription: 'This is the first task of the project',
-			project: 'MissionTracker',
-			startTime: '08:05:10',
-			endTime: '09:34:44',
-			totalTime: '01:29:34',
-			date: new Date('2021-01-11T00:00:00'),
-			priority: 'Low',
-		},
-		{
-			id: 'ddeeff',
-			taskDescription: 'Second Task',
-			project: 'MissionTracker',
-			startTime: '09:34:44',
-			endTime: '10:02:39',
-			totalTime: '00:27:55',
-			date: new Date('2021-01-12T00:00:00'),
-			priority: 'Low',
-		},
-		{
-			id: 'gghhii',
-			taskDescription: 'Third Task',
-			project: 'MissionTracker',
-			startTime: '10:02:39',
-			endTime: '13:04:11',
-			totalTime: '03:01:16',
-			date: new Date('2021-01-12T00:00:00'),
-			priority: 'Medium',
-		},
+		// {
+		// 	id: 'aabbcc',
+		// 	description: 'This is the first task of the project',
+		// 	project: 'MissionTracker',
+		// 	startTime: '08:05:10',
+		// 	endTime: '09:34:44',
+		// 	totalTime: '01:29:34',
+		// 	date: new Date('2021-01-11T00:00:00'),
+		// 	priority: 'Low',
+		// },
+		// {
+		// 	id: 'ddeeff',
+		// 	description: 'Second Task',
+		// 	project: 'MissionTracker',
+		// 	startTime: '09:34:44',
+		// 	endTime: '10:02:39',
+		// 	totalTime: '00:27:55',
+		// 	date: new Date('2021-01-12T00:00:00'),
+		// 	priority: 'Low',
+		// },
+		// {
+		// 	id: 'gghhii',
+		// 	description: 'Third Task',
+		// 	project: 'MissionTracker',
+		// 	startTime: '10:02:39',
+		// 	endTime: '13:04:11',
+		// 	totalTime: '03:01:16',
+		// 	date: new Date('2021-01-12T00:00:00'),
+		// 	priority: 'Medium',
+		// },
 	]);
 
-	// const inputChangedHandler = (event, inputId) => {};
+	useEffect(() => {
+		axios
+			.get(
+				'https://mission-time-tracker-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
+			)
+			.then(response => {
+				const entriesList = [];
+				const fetchedTasksObject = Object.entries(response.data);
+
+				fetchedTasksObject.forEach(el =>
+					entriesList.push({ ...response.data[el], id: el })
+				);
+
+				const updatedTaskList = [];
+				const fetchedTasks = fetchedTasksObject[0];
+				updatedTaskList.push(fetchedTasks[1]);
+				setTasksList(updatedTaskList);
+			})
+			.catch(error => console.log(error))
+			.finally();
+	}, []);
 
 	const handleDelete = id => {
 		const newTasksList = tasksList.filter(item => item.id !== id);
 		setTasksList(newTasksList);
 	};
 
+	let tasks = <Spinner />;
+
+	if (tasksList) {
+		tasks = tasksList.map(task => (
+			<TaskItem
+				key={Math.floor(Math.random() * 10000)}
+				taskId={task.id}
+				description={task.description}
+				project={task.project}
+				startTimeInput={task.startTime}
+				endTimeInput={task.endTime}
+				totalTimeInput={task.totalTime}
+				date={task.date}
+				priority={task.priority}
+				deleteItem={() => handleDelete(task.id)}
+			/>
+		));
+	}
+
 	return (
 		<>
 			<Task />
-			<div className={classes.TaskItemList}>
-				{tasksList.map(task => (
-					<TaskItem
-						key={Math.floor(Math.random() * 10000)}
-						taskId={task.id}
-						description={task.taskDescription}
-						project={task.project}
-						startTimeInput={task.startTime}
-						endTimeInput={task.endTime}
-						totalTimeInput={task.totalTime}
-						date={task.date}
-						priority={task.priority}
-						deleteItem={() => handleDelete(task.id)}
-						// onChange={inputChangedHandler}
-					/>
-				))}
-			</div>
+			<div className={classes.TaskItemList}>{tasks}</div>
 		</>
 	);
 };
