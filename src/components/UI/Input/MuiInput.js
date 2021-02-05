ec485a64bdde0833c54bad9162f75563070a440d
Ori M. K <kosman.ori@gmail.com>
2021-02-05 14:26:40 +0200

    Replaced with the default mui controls

--- src/components/UI/Input/MuiInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-
-const MuiInput = props => {
-	const { id, label, size, value } = props;
-
-	return <TextField id={id} label={label} size={size} value={value} />;
-};
-export default MuiInput;
