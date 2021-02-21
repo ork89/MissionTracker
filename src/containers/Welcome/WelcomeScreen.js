a639fa63c57ae019c22b22d307a49523e5c8508a
Ori M. K <kosman.ori@gmail.com>
2021-02-21 22:36:13 +0200

    Small refactoring

--- src/containers/Welcome/WelcomeScreen.js
+++ src/containers/Welcome/WelcomeScreen.js
@@ -2,19 +2,14 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 
 import MissionTrackerLogo from '../../assets/MissionTrackerLogo.png';
-import welcome from '../../assets/welcome.jpg';
 import classes from './WelcomeScreen.module.css';
 
 const WelcomeScreen = () => {
 	return (
 		<div className={classes.container}>
-			<header>
-				Navbar with login and signup buttons
-			</header>
+			<header>Navbar with login and signup buttons</header>
 			<img src={MissionTrackerLogo} className={classes.logoImage} alt='MT-Logo' />
-			<Typography variant='h5'>
-				Designed and built by Ori M. Kosman
-			</Typography>
+			<Typography variant='h5'>Designed and built by Ori M. Kosman</Typography>
 		</div>
 	);
 };
